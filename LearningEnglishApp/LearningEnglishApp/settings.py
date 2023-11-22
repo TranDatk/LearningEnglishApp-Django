45d0e49fc3283d1aff2f74578411eeb018c38250
TranDatk <hgooshvhd123@gmail.com>
2023-11-22 23:54:57 +0700

    Update settings.py

--- LearningEnglishApp/LearningEnglishApp/settings.py
+++ LearningEnglishApp/LearningEnglishApp/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'LearningEnglishApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learningenglishdb',
+        'USER': 'root',
+        'PASSWORD': '25052002',
+        'HOST': ''
     }
 }
 
+AUTH_USER_MODEL = 'LearningEnglish.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
