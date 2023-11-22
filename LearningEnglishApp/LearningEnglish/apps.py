041bca2f2a07fd7ac82cbc8f28e0fdf703ee5ad8
TranDatk <hgooshvhd123@gmail.com>
2023-11-22 17:03:49 +0700

    Initial commit

---
+++ LearningEnglishApp/LearningEnglish/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LearningenglishConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LearningEnglish'
---
+++ README.md
@@ -0,0 +1,2 @@
+# LearningEnglishApp-Django
+ 
