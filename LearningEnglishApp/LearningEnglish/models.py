1e05bc69d51c883272bd7b088ea1168cec259b48
TranDatk <hgooshvhd123@gmail.com>
2023-11-22 23:54:42 +0700

    22/11/2023

--- LearningEnglishApp/LearningEnglish/models.py
+++ LearningEnglishApp/LearningEnglish/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='uploads/%Y/%m')
+
+class ItemBase(models.Model):
+    class Meta:
+        abstract = True
+
+    name = models.CharField(max_length=100, null=False, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Topic(ItemBase):
+    class Meta:
+        db_table = 'topic'
+        ordering = ["created_date"]
+
+    def __str__(self):
+        return self.name
+
+class Course(ItemBase):
+    class Meta:
+        db_table = 'course'
+        ordering = ["created_date"]
+    image = models.ImageField(upload_to='courses/%Y/%m', default=None)
+    description = models.TextField(null=True, blank=True)
+    topic = models.ManyToManyField(Topic, related_name="topics",null=True, blank=True)
+
+class Lesson(ItemBase):
+    class Meta:
+        unique_together = ('name','fk_courses')
+        db_table = 'lesson'
+        ordering = ["created_date"]
+    index = models.IntegerField(null = False)
+    description = models.TextField(null=True, blank=True)
+    fk_courses = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons")
